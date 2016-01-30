03f326c4667b723c8c88a9f9e9a8a7444c420b30
Jóhannes Gunnar Þorsteinsson <johannesg@johannesg.com>
2016-01-30 01:12:37 +0000

    Simple car flipper, if stuck

---
+++ Assets/CarFlipper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarFlipper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            transform.Translate(Vector3.up * 5, Space.World);
+            transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+	}
+}
