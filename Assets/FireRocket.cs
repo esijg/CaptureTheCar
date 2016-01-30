a108b061884dedbf3e16440c534c5b7cd258b704
bennithor <bennithor@gmail.com>
2016-01-30 17:36:13 +0000

    Rockets and shit

---
+++ Assets/FireRocket.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireRocket : MonoBehaviour {
+
+    public GameObject rock;
+
+
+	// Use this for initialization
+	void Start () {
+	    
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Fire" + gameObject.GetComponentInParent<playerCollision>().playerNumber.ToString()))
+        {
+            Instantiate(rock, transform.position, Quaternion.identity);
+
+        }
+	}
+}
