a9c499e960d5dd1a3f24de450d5452cfae79bddf
Jóhannes Gunnar Þorsteinsson <johannesg@johannesg.com>
2016-01-31 14:18:37 +0000

    cars, terrain, music, crap

--- Assets/CarFlipper.cs
+++ Assets/CarFlipper.cs
@@ -3,17 +3,23 @@ using System.Collections;
 
 public class CarFlipper : MonoBehaviour {
 
+    float timer;
 	// Use this for initialization
 	void Start () {
-	
+        timer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetButtonDown("Flip" + gameObject.GetComponentInParent<playerCollision>().playerNumber.ToString()) && timer < 0.1)
         {
             transform.Translate(Vector3.up * 5, Space.World);
             transform.localEulerAngles = new Vector3(0, 0, 0);
+            timer = 5;
+        }
+        if(timer>=0)
+        {
+            timer = timer - Time.deltaTime;
         }
 	}
 }
