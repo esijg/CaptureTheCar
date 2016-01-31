a23a3d927a0638b28618fac757771943a0fc3dd0
Jóhannes Gunnar Þorsteinsson <johannesg@johannesg.com>
2016-01-31 01:25:48 +0000

    bigger rocket explosions

--- Assets/explosion.cs
+++ Assets/explosion.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class explosion : MonoBehaviour {
 
+    public float AreaOfEffect = 6.0f;
+    public float Strength = 500;
+
 	// Use this for initialization
 	void Start () {
-        GetComponent<Rigidbody>().AddExplosionForce(10000, transform.position, 100, 3.0f);
+        GetComponent<Rigidbody>().AddExplosionForce(10000, transform.position, Strength, AreaOfEffect);
     }
 
     // Update is called once per frame
