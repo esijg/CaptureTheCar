1ce7462dd21951fb43fd2d0ac49342dd639b523a
Jóhannes Gunnar Þorsteinsson <johannesg@johannesg.com>
2016-02-13 15:39:26 +0000

    Migrating repo to gitlab.

--- Assets/Scripts/explosion.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class explosion : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-        Destroy(gameObject, 4.0f);
-    }
-}
